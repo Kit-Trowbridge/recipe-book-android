e4cd2df8e150f143a46d7031af81da662c0181b8
kit-trowbridge <kittrow.dev@gmail.com>
2026-07-17 12:56:20 +0100

    style Title card

--- app/src/main/java/com/example/recipebook/screens/RecipeDetailsScreen.kt
+++ app/src/main/java/com/example/recipebook/screens/RecipeDetailsScreen.kt
@@ -1,23 +1,31 @@
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.recipebook.R
 import com.example.recipebook.data.Recipe
 
 @Composable
 fun RecipeDetailsScreen(recipe: Recipe, modifier: Modifier = Modifier){
     Column(){
         RecipeTitleCard(recipe = recipe)
+//        Spacer(
+//            Modifier.height(28.dp)
+//        )
         RecipeDetails(recipe = recipe)
     }
 }
@@ -25,14 +33,25 @@ fun RecipeDetailsScreen(recipe: Recipe, modifier: Modifier = Modifier){
 
 @Composable
 fun RecipeTitleCard(recipe: Recipe, modifier: Modifier = Modifier) {
-    Box(modifier = modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+    ) {
         Image(
-            painter = painterResource(recipe.image)
+            painter = painterResource(recipe.image),
+            recipe.title
         )
-        Text(
-            text = recipe.title,
-            fontSize = 28.sp
-        )
+        Surface(
+            color = Color.White.copy(alpha = 0.8F)
+        ) {
+            Text(
+                text = recipe.title,
+                textAlign = TextAlign.Center,
+                fontSize = 28.sp,
+                modifier = modifier
+                    .padding(all = 14.dp)
+            )
+        }
     }
 
 }
