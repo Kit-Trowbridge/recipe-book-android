839f63d040ef6afe98ed74315ee6749f720b9bb2
kit-trowbridge <kittrow.dev@gmail.com>
2026-07-17 12:14:54 +0100

    RecipeDetailsScreen set up

--- app/src/main/java/com/example/recipebook/screens/RecipeDetailsScreen.kt
+++ app/src/main/java/com/example/recipebook/screens/RecipeDetailsScreen.kt
@@ -1,7 +1,54 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.recipebook.data.Recipe
 
 @Composable
-fun RecipeDetailsScreen(recipe: Recipe){
+fun RecipeDetailsScreen(recipe: Recipe, modifier: Modifier = Modifier){
+    Column(){
+        RecipeTitleCard(recipe = recipe)
+        RecipeDetails(recipe = recipe)
+    }
+}
 
+
+@Composable
+fun RecipeTitleCard(recipe: Recipe) {
+    Text(
+        text = recipe.title
+    )
+}
+
+@Composable
+fun RecipeDetails(recipe: Recipe, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(){
+            Text(
+                text = recipe.ingredients.joinToString(),
+                fontSize = 12.sp
+            )
+        }
+        Row(){}
+
+    }
+}
+
+val previewRecipe = recipes.last()
+
+@Preview
+@Composable
+fun RecipeDetailsScreenPreview(){
+    Column(){
+        RecipeTitleCard(recipe = previewRecipe)
+        RecipeDetails(recipe = previewRecipe)
+    }
 }
